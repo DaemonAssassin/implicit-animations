a81fde786a2b5a525667fa855ce665a8245cc46a
Mateen <mateendev3@gmail.com>
2022-08-27 16:22:09 +0500

    Learned AnimatedPhysicalModel

---
+++ lib/animated_physical_mode.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../../utils/helper_widgets.dart';
+
+class MyAnimatedPhysicalModel extends StatefulWidget {
+  const MyAnimatedPhysicalModel({Key? key}) : super(key: key);
+
+  @override
+  State<MyAnimatedPhysicalModel> createState() =>
+      MyAnimatedPhysicalModelState();
+}
+
+class MyAnimatedPhysicalModelState extends State<MyAnimatedPhysicalModel> {
+  double _elevation = 0.0;
+  Color _color = Colors.lime;
+  Color _shadowColor = Colors.black;
+  bool isFirst = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedPhysicalModel(
+          duration: const Duration(seconds: 1),
+          shape: BoxShape.rectangle,
+          elevation: _elevation,
+          color: _color,
+          shadowColor: _shadowColor,
+          child: _buildContainer(),
+        ),
+        addVerticalSpace(30.0),
+        buildButton(
+          text: 'Slide up',
+          onPressed: () {
+            _change();
+          },
+        ),
+      ],
+    );
+  }
+
+  void _change() {
+    setState(() {
+      _elevation = isFirst ? 10.0 : 0.0;
+      _color = isFirst ? Colors.black : Colors.lime;
+      _shadowColor = isFirst ? Colors.red : Colors.transparent;
+      isFirst = !isFirst;
+    });
+  }
+
+  Widget _buildContainer() {
+    return const SizedBox(
+      width: 100.0,
+      height: 100.0,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:implicit_animations_flutter/widgets/animated_widgets/animated_slide.dart';
+import 'package:implicit_animations_flutter/animated_physical_mode.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: MyAnimatedSlide(),
+          child: MyAnimatedPhysicalModel(),
         ),
       ),
     );
